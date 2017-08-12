4966e1d5c897fda947e23d5f112578b5c0aaf575
yuebiaochen <2923878201@qq.com>
2017-08-13 02:15:04 +0800

    Update CommonRequestIdManager.js

--- CommonRequestIdManager.js
+++ CommonRequestIdManager.js
@@ -18,6 +18,10 @@ function CommonRequestIdManager() {
         },
         // 清除请求的id，把id加一，导致前面发出的请求不可用
         clearRequestId: function() {
+          // requestId等于1說明還沒有發出過請求，所以不需要請求請求id
+          if (this.requestId === 1) {
+              return;
+          }
           return this.addRequestId();
         },
         addRequestId: function() {
@@ -41,4 +45,3 @@ function CommonRequestIdManager() {
           }
         }
     }
-  
